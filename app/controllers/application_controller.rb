e1b2f75599c300a3e65cf873ef903b933c698669
Alex Ju <aju217@gmail.com>
2022-12-12 13:54:49 -0500

    React setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    # checking if cookies work, delete later
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # checking if cookies work, delete later
   get '/hello', to: 'application#hello_world'
 end
