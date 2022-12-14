2813b1b861c5fd4e92c6410e864fc360ba7b2946
Alex Ju <aju217@gmail.com>
2022-12-14 12:57:27 -0500

    changed session params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
     # currently set to login using email/password combo
     def create
-        user = User.find_by(username: params[:username])
+        user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, status: :created
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_155650) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
