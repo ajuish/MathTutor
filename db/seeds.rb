e2965825543712c9d2bf0a69950c43b17fe17b10
Alex Ju <aju217@gmail.com>
2022-12-14 11:21:39 -0500

    changed username to email

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(username: 'alex', password: 'abcd1234')
-User.create(username: "jo", password: 'password')
+User.create(email: 'alex@gmail.com', password: 'abcd1234')
+User.create(email: "jo@gmail.com", password: 'password')
