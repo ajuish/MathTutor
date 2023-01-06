57b73d94e2140698e6a57978aac7579387a03b12
Alex Ju <aju217@gmail.com>
2023-01-06 14:07:27 -0500

    added code for division

--- client/src/Fundamentals.jsx
+++ client/src/Fundamentals.jsx
@@ -13,6 +13,8 @@ function Fundamentals(){
    const [currentOperation, setCurrentOperation] = useState('')
    const [symbol, setSymbol] = useState('')
    const [userAnswer, setUserAnswer] = useState('')
+   const [division, setDivision] = useState(1)
+   const [numerator, setNumerator] = useState('tens')
    const [score, setScore] = useState({
       addition: [0,0],
       subtraction:[0,0],
@@ -69,18 +71,67 @@ function Fundamentals(){
    let question
    
    if (symbol === '-'){
-      if (negative === false){
-         if (num2 > num1){
-            question = `${num2} ${symbol} ${num1}`
+      {
+         if (negative === false){
+            if (num2 > num1){
+               question = `${num2} ${symbol} ${num1}`
+            }
+            else {
+               question = `${num1} ${symbol} ${num2}`
+            }
          }
-         else {
+         else 
             question = `${num1} ${symbol} ${num2}`
-         }
       }
-      else 
-         question = `${num1} ${symbol} ${num2}`
    }
-   else 
+   else if (symbol==='/')
+      if (numerator === 'tens')
+         {
+         if (division === 1)
+            question = `${getRandomInt(100)} ${symbol} ${1}`
+         else if (division === 2)
+            {question = `${getRandomInt(50)*2} ${symbol} ${2}`}
+         else if (division === 3)
+            {question = `${getRandomInt(34)*3} ${symbol} ${3}`}
+         else if (division === 4)
+            {question = `${getRandomInt(25)*4} ${symbol} ${4}`}
+         else if (division === 5)
+            {question = `${getRandomInt(20)*5} ${symbol} ${5}`}
+         else if (division === 6)
+            {question = `${getRandomInt(17)*6} ${symbol} ${6}`}
+         else if (division === 7)
+            {question = `${getRandomInt(15)*7} ${symbol} ${7}`}
+         else if (division === 8)
+            {question = `${getRandomInt(13)*8} ${symbol} ${8}`}
+         else if (division === 9)
+            {question = `${getRandomInt(12)*9} ${symbol} ${9}`}
+            else if (division === 10)
+            {question = `${getRandomInt(10)*10} ${symbol} ${10}`}
+         }
+      else 
+      {
+         if (division === 1)
+            question = `${getRandomInt(1000)} ${symbol} ${1}`
+         else if (division === 2)
+            {question = `${getRandomInt(500)*2} ${symbol} ${2}`}
+         else if (division === 3)
+            {question = `${getRandomInt(334)*3} ${symbol} ${3}`}
+         else if (division === 4)
+            {question = `${getRandomInt(250)*4} ${symbol} ${4}`}
+         else if (division === 5)
+            {question = `${getRandomInt(200)*5} ${symbol} ${5}`}
+         else if (division === 6)
+            {question = `${getRandomInt(167)*6} ${symbol} ${6}`}
+         else if (division === 7)
+            {question = `${getRandomInt(143)*7} ${symbol} ${7}`}
+         else if (division === 8)
+            {question = `${getRandomInt(126)*8} ${symbol} ${8}`}
+         else if (division === 9)
+            {question = `${getRandomInt(112)*9} ${symbol} ${9}`}
+            else if (division === 10)
+            {question = `${getRandomInt(100)*10} ${symbol} ${10}`}
+         }
+   else
       {question = `${num1} ${symbol} ${num2}`}
 
    //checks solution onSubmit
@@ -167,19 +218,87 @@ function Fundamentals(){
                <br></br>
             </div>
          </Card>
-         <Card className='ui centered grid' onChange={(e)=> setDigits(Number(e.target.value))}>
-            <div className='center aligned content'>
+         <Card className='ui centered grid'>
+         {(currentOperation === 'division') ? 
+            <div>
+               <h4>Numerator:</h4>
+               <div 
+                  onChange={(e) => setNumerator(e.target.value)}
+                  style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}
+               >
+                  <div>
+                     <input type='radio' value='tens' name='digits' defaultChecked/> Two Digits
+                  </div>
+                  <div>
+                     <input type='radio' value='hundreds' name='digits'/> Three Digits
+                  </div>
+               </div>
+               <br></br>
+               <div>
+                  <h4>Divide By:</h4>
+                  <div 
+                     onChange={(e) => setDivision(Number(e.target.value))}
+                     style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 20 }}
+                  >
+                     <div>
+                        <div>
+                           <input type='radio' value='1' name='division' defaultChecked/> 1
+                        </div>
+                        <div>
+                           <input type='radio' value='2' name='division'/> 2
+                        </div>
+                        <div>
+                           <input type='radio' value='3' name='division'/> 3
+                        </div>
+                        <div>
+                           <input type='radio' value='4' name='division'/> 4
+                        </div>
+                        <div>
+                           <input type='radio' value='5' name='division'/> 5
+                        </div>
+                     </div>
+                     <div>
+                        <div>
+                           <input type='radio' value='6' name='division'/> 6
+                        </div>
+                        <div>
+                           <input type='radio' value='7' name='division'/> 7
+                        </div>
+                        <div>
+                           <input type='radio' value='8' name='division'/> 8
+                        </div>
+                        <div>
+                           <input type='radio' value='9' name='division'/> 9
+                        </div>
+                        <div>
+                           <input type='radio' value='10' name='division'/> 10
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <br></br>
+            </div>
+            
+            :
+            
+            <div 
+               className='center aligned content' 
+               onChange={(e)=> setDigits(Number(e.target.value))}
+            >
                <h4>How many digits?</h4>
-               <div>
-                  <input type='radio' value='10' name='digits' defaultChecked/> Single Digit
-               </div>
-               <div>
-                  <input type='radio' value='100' name='digits'/> Two Digits
-               </div>
-               <div>
-                  <input type='radio' value='1000' name='digits'/> Three Digits
+               <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
+                  <div>
+                     <input type='radio' value='10' name='digits' defaultChecked/> Single Digit
+                  </div>
+                  <div>
+                     <input type='radio' value='100' name='digits'/> Two Digits
+                  </div>
+                  <div>
+                     <input type='radio' value='1000' name='digits'/> Three Digits
+                  </div>
                </div>
             </div>
+         }
            {(currentOperation === 'subtraction') ? 
                <div className='ui center aligned'> 
                   <h4>Include Negative Results?</h4>
